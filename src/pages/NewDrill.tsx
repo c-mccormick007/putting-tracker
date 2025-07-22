8c4abe05949f8321821846ae24dec81b9d585a85
c-mccormick007 <c.mccormick1295@gmail.com>
2025-07-22 14:14:33 -0500

    new drill form started, add/remove distance button implemented

--- src/pages/NewDrill.tsx
+++ src/pages/NewDrill.tsx
@@ -1,3 +1,35 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { supabase } from "../lib/supabase"
+
 export default function NewDrill(){
-    return <h1>NewDrill</h1>
+    const [distances, setDistances] = useState<number[]>([15]);
+    
+    return (
+        <div>
+            <h1>Create a Drill</h1>
+            <div>
+                <label>Distances (feet)</label>
+                {distances.map((dist, i) => (
+                    <div key={i}>
+                        <input 
+                            type="number"
+                            value={dist}
+                            onChange={e => {
+                                const copy = [...distances];
+                                copy[i] = Number(e.target.value);
+                                setDistances(copy);
+                            }} 
+                        />
+                        <button onClick={() => {
+                            setDistances(distances.filter((_, del) => del !== i))
+                        }}>X</button>
+                    </div>
+                ))}
+            <button
+                onClick={() => setDistances([...distances, 0])}
+            >+ Add Distance</button>
+            </div>
+        </div>
+    )
 }
