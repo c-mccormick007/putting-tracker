b576fb4203babeba5e13536af640412295f05bfc
c-mccormick007 <c.mccormick1295@gmail.com>
2025-07-22 12:26:50 -0500

    navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
+        <nav className="flex items-center gap-4 p-4 bg-neutral-900 text-white">
+            <Link to="/" className="font-semibold">Home</Link>
             <Link to="/drill/new">New Drill</Link>
             <Link to="/history">History</Link>
         </nav>
