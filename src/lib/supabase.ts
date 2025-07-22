8011f74f9e4ebbaf91b971e096412e6c1567faa2
c-mccormick007 <c.mccormick1295@gmail.com>
2025-07-22 12:04:09 -0500

    hook supabase up

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    import.meta.env.VITE_SUPERBASE_URL!,
+    import.meta.env.VITE_SUPERBASE_ANON_KEY!
+)
